a5aadbed984f41af11ad5f5e1e17c5eac21b968a
Ashwanth222 <ashwanthrao22@gmail.com>
2023-06-08 10:06:19 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MyCustomCountingHook } from "./hooks/MyCustomHooks";
 
-function App() {
+const App = () => {
+  const { count, incrementCount } = MyCustomCountingHook();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>The state is {count}</p>
+      <button onClick={() => incrementCount()}>Increment State</button>
     </div>
   );
-}
+};
 
 export default App;
